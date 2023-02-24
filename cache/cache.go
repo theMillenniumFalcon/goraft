136a33b19b1179fca107adc50bf87d23e78b98b0
theMillenniumFalcon <nishankpr2002@gmail.com>
2023-02-24 10:08:54 +0530

    added some basic functions

--- cache/cache.go
+++ cache/cache.go
@@ -1,6 +1,9 @@
 package cache
 
-import "sync"
+import (
+	"sync"
+	"time"
+)
 
 type Cache struct {
 	lock sync.RWMutex
@@ -11,4 +14,24 @@ func New() *Cache {
 	return &Cache{
 		data: make(map[string][]byte),
 	}
+}
+
+func (c *Cache) Set(key, value []byte, timeToLive time.Duration) error {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+}
+
+func (c *Cache) Has(key []byte) bool {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+}
+
+func (c *Cache) Get(key []byte) ([]byte, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+}
+
+func (c *Cache) Delete(key []byte) error {
+	c.lock.Lock()
+	defer c.lock.Unlock()
 }
