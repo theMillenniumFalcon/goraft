32c9a2a094bb857719eb1fead202218f63d39ef7
theMillenniumFalcon <nishankpr2002@gmail.com>
2023-02-25 20:02:15 +0530

    added followers in Server

--- server.go
+++ server.go
@@ -17,13 +17,14 @@ type ServerOptions struct {
 
 type Server struct {
 	ServerOptions
-
-	cache cache.Cacher
+	followers map[net.Conn]struct{}
+	cache     cache.Cacher
 }
 
 func NewServer(options ServerOptions, _cache cache.Cacher) *Server {
 	return &Server{
 		ServerOptions: options,
+		followers:     make(map[net.Conn]struct{}),
 		cache:         _cache,
 	}
 }
