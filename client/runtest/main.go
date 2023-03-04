d78b27ac9db65973c44a0001d6196f7ed14ae53c
theMillenniumFalcon <nishankpr2002@gmail.com>
2023-03-04 16:25:44 +0530

    made hashicorp raft work with goraft

--- client/runtest/main.go
+++ client/runtest/main.go
@@ -4,13 +4,53 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
+	"os"
 	"time"
 
+	"github.com/hashicorp/raft"
 	"github.com/theMillenniumFalcon/goraft/client"
 )
 
+type Server struct {
+	raft *raft.Raft
+}
+
 func main() {
-	SendStuff()
+	var (
+		config             = raft.DefaultConfig()
+		finiteStateMachine = &raft.MockFSM{}
+		stableStore        = raft.NewInmemStore()
+		logStore           = raft.NewInmemStore()
+		snapShotStore      = raft.NewInmemSnapshotStore()
+		timeout            = time.Second * 5
+	)
+
+	config.LocalID = "NPID"
+
+	ips, err := net.LookupIP("localhost")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(ips) == 0 {
+		log.Fatalf("localhost did not resolve to any IPs")
+	}
+	addr := &net.TCPAddr{IP: ips[0], Port: 4000}
+
+	fmt.Println(addr)
+
+	tr, err := raft.NewTCPTransport(":3000", addr, 10, timeout, os.Stdout)
+	if err != nil {
+		log.Fatal("tcp net failed", err)
+	}
+	r, err := raft.NewRaft(config, finiteStateMachine, stableStore, logStore, snapShotStore, tr)
+	if err != nil {
+		log.Fatal("failed to create new raft: ", err)
+	}
+
+	fmt.Printf("%+v\n", r)
+
+	select {}
 }
 
 func SendStuff() {
--- go.mod
+++ go.mod
@@ -5,7 +5,13 @@ go 1.18
 require github.com/stretchr/testify v1.8.2
 
 require (
+	github.com/armon/go-metrics v0.0.0-20190430140413-ec5e00d3c878 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/hashicorp/go-hclog v0.9.1 // indirect
+	github.com/hashicorp/go-immutable-radix v1.0.0 // indirect
+	github.com/hashicorp/go-msgpack v0.5.5 // indirect
+	github.com/hashicorp/golang-lru v0.5.0 // indirect
+	github.com/hashicorp/raft v1.3.11 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
